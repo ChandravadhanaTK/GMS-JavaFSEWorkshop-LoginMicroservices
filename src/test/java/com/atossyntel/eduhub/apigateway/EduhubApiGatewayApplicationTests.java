d10019d6aea9a577f3d57d5a0114cd433265c4ae
MuhammadJeelan24 <shaikjeelan123@gmail.com>
2021-08-16 09:29:57 +0530

    initial commit

---
+++ src/test/java/com/atossyntel/eduhub/apigateway/EduhubApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atossyntel.eduhub.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduhubApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
